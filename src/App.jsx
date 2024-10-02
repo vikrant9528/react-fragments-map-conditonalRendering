e7226c55247c8bd33bb610ebc7e4595aa7b70ae6
vikrant9528 <vikrantchaudhary258@gmail.com>
2024-10-02 16:46:09 +0530

    passing children using {props.children}

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Fooditems from "./components/Fooditems";
 import ErrorMessage from "./components/ErrorMessage";
 import "./App.css"
+import Container from "./components/Container";
 function App() {
   let foodItems = ["sabji","Green vegetables","Roti","Salad","Milk","ghee"];
   // let foodItems = [];
   return (
     <>
+    <Container>
       <h1 className="food-heading">Healthy Foods</h1>
       <ErrorMessage items={foodItems} />
       <Fooditems items={foodItems}/>
+      </Container>
+
+      <Container>
+        <p>here is the list of healthy foods that are good for your health</p>
+      </Container>
     </>
   );
 }
